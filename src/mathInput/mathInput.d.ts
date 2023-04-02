46799bcae6f608b84cd884105aef46ab47263e43
Robin <robingranda@gmail.com>
2023-04-02 17:57:34 +0200

    fonts

--- src/mathInput/mathInput.d.ts
+++ src/mathInput/mathInput.d.ts
@@ -1,6 +1,5 @@
 import React from "react";
 import { KeyboardProps } from "../keyboard/keyboard";
-import "mathquill4keyboard/build/mathquill.css";
 type Props = {
     keyboardProps?: KeyboardProps;
     setValue?: (s: string) => void;
--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,18 @@ module.exports = (env, argv) => {
           // include: path.join(__dirname, "src"),
           use: ["style-loader", "css-loader", "postcss-loader"],
         },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]",
+                outputPath: "fonts/",
+              },
+            },
+          ],
+        },
       ],
     },
     plugins: [
