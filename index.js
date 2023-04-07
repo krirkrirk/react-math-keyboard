12d8e6fafa60f2bc3524dc73f8c8714b8c7151f6
Robin <robingranda@gmail.com>
2023-04-07 18:56:30 +0200

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./src/style.css";
+import "mathquill4keyboard/build/mathquill.css";
+import { Example } from "./examples/example";
+
+ReactDOM.render(<Example />, document.getElementById("root"));
