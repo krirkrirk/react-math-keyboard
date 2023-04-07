61f51dd962ff8daa6f970ac6aa903deed53935e0
Robin <robingranda@gmail.com>
2023-04-07 19:05:23 +0200

    test

--- index.js
+++
@@ -1,7 +0,0 @@
-const React = require("react");
-const ReactDOM = require("react-dom");
-require("./src/style.css");
-require("mathquill4keyboard/build/mathquill.css");
-const Example = require("./examples/example");
-console.log(Example);
-ReactDOM.render(<Example.Example />, document.getElementById("root"));
